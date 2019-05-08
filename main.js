66d25762f22361d3691bce04ceb81c411e798383
Vlad Kostenko <kost443562@gmail.com>
2019-05-08 22:55:56 +0300

    start

---
+++ main.js
@@ -0,0 +1,15 @@
+// function clock (){
+//     let delay = 10;
+//     i=0;
+//     let elem = document.getElementsByClassName('main_img_1');
+//     bottom = elem.offsetTop;
+
+//     if(i<10){
+//         elem.style.top= bottom + 20 + 'px';
+//     }else{
+//         clearInterval(timer);
+//     }
+//     i++;
+// };
+
+// var timer = setInterval(startTimer. delay)
